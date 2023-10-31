11e0128b1f5e4dd7f4cd640c39514b075be08c0b
Minghai Chen <chenminghai79@gmail.com>
2023-10-31 18:12:17 +0000

    prueba js

---
+++ codeKata1.js
@@ -0,0 +1,31 @@
+// let namedCollections = ["Pepe", "Juan", "Carlos", "Pedro"];
+// console.log(namedCollections);
+
+// console.log(namedCollections[0]);
+
+
+// let enumu = new Set([1, 7, true, "juan"]);
+// enumu.add("carlos");
+// console.log(enumu);
+
+
+// let mapeo = new Map([
+//   [1, "juan"],
+//   [2, "pedro"],
+// ]);
+
+// mapeo.set(1, "carlos")
+
+// console.log(mapeo.get(1));
+
+// let objeto = {
+//   nombre: "juan",
+//   edad: 35,
+// };
+// console.log(objeto.edad);
+
+
+// let currentDay = new Date();
+// console.log(currentDay);
+// let secondCurrentDay = new Date();
+// console.log(secondCurrentDay);
